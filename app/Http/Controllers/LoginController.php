44c4d8a3d107ee25d9ea32ae090cd104dce24304
chl <1032326058@qq.com>
2019-04-26 01:05:47 +0800

    make page:login|register|user

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    function index()
+    {
+        return view('login.index');
+    }
+    function login()
+    {
+
+    }
+    function logout()
+    {
+
+    }
+}
